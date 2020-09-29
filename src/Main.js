125050040209be91d5ff405fca95bb6e9d9d716c
unknown <baitik-jump@yandex.ru>
2020-09-29 13:48:13 +0300

    gg

--- src/Main.js
+++ src/Main.js
@@ -8,60 +8,22 @@ export const Main = (props) =>{
 
   return(
     <div>
-    <div className="container">
-    	<div className="poloska">
+    <div class="logo">
+      <a href=""><img width="40" src={ logo }/></a>
+    </div>
+    <div class="logotext">
+      HistoryTime
 
-    	</div>
-    	<header>
-    			<div className="logo">
-    				  <a href=""><img width="40" src={logo}/></a>
-    			</div>
-    			<div className="logotext">
-            <p>
-    				  HistoryTime
-    			  </p>
-    			</div>
-    			<div className="avatar">
-    			</div>
-    	</header>
-    	<div className="container1">
-    		<div className="text">
-    			<p>ПРОЙДИ<br>ПУТЬ ЖИЗНИ</br> ИЗВЕСТНОЙ <br>ЛИЧНОСТИ</br></p>
-    			<div>
-    			<svg className="strelka-left-3" viewBox="0 0 5 9">
-    			   < path d="M0.419,9.000 L0.003,8.606 L4.164,4.500 L0.003,0.394 L0.419,0.000 L4.997,4.500 L0.419,9.000 Z" ></path>
-    			</svg>
-
-
-    			<svg className="strelka-right-3" viewBox="0 0 5 9">
-    					    <path d="M0.419,9.000 L0.003,8.606 L4.164,4.500 L0.003,0.394 L0.419,0.000 L4.997,4.500 L0.419,9.000 Z" ></path>
-    			</svg>
-
-    			</div>
-    			<div className="search1">
-
-    			 	<form onsubmit="event".preventDefault() role="search">
-    			  		<label for="search">Search for stuff</label>
-    			  		<input id="search" type="search" placeholder="Search..." autofocus required />
-    			  		<button type="submit">Go</button>
-    				</form>
-    			</div>
-    		</div>
-    		<div className="scroll_bar">
-    				<a href="file:///C:/Users/Baytik-228/Desktop/HistoryTime/TimeLine.html"></a><img height="125%" src={steve}/>
-    		</div>
-    		<div className="contact">
-    			<div className="vk"><a href="https://vk.com/rgilyazow"></a></div>
-    			<div className="yt"><a href=""></a></div>
-    			<div className="inst"><a href=""></a></div>
-
-    		</div>
-    	</div>
-
+    
+    </div>
+    <div class="avatar">
     </div>
 
-      <Search/>
-      <div onClick={() => props.onPageChange(1)}>Подробнее...</div>
-    </div>
+<div class="container1">
+  <div class="text">
+    ПРОЙДИ ПУТЬ ЖИЗНИ ИЗВЕСТНОЙ ЛИЧНОСТИ
+</div>
+</div>
+
   )
 }
